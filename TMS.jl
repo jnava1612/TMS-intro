3fd1c6e6c838897acd07608ec05027aa54263d10
Jorge E Nava <navajorgeeduardo@gmail.com>
2026-02-12 12:45:21 -0300

    added some of the model parameters

--- TMS.jl
+++ TMS.jl
@@ -3,3 +3,23 @@ using LinearAlgebra
 using Random
 using Printf
 using ITensors.HDF5
+
+function main()
+    N = 20
+    maxD = 10
+    minD = 20
+
+    @show "Generating random MPS with N = $N, maxD = $maxD, minD = $minD"
+
+    E0 = -1.1
+    max_op = 100 # maximum number of iterations
+
+    ξthre = 0.1 # threshold for updating ξ
+
+    ϵ = 1e-10 # convergence threshold for energy
+
+    sites = siteinds("S=1/2", N, conserve_qns=false)
+
+    @show sites[1]
+
+end
