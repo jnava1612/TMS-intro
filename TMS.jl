0c8dfd27199a708b4ae10fc2fdedc1dcbeae2a63
Jorge E Nava <navajorgeeduardo@gmail.com>
2026-02-19 15:34:53 -0300

    Added plot verification, the code isn't working in the way expected

--- TMS.jl
+++ TMS.jl
@@ -3,15 +3,56 @@ using LinearAlgebra
 using Random
 using Printf
 using ITensors.HDF5
+using CairoMakie
+
+# This version of the code is not yet capable of reproducing the tower of scars that is expected on the PXP model.
+# It is only capable of finding the lowest energy scar state and succesfully reproducing its values.
+# Further changes implementd on the code will ensure the ability to find the whole tower of scar states, and not just the lowest energy one.
 
 function main()
-    N = 20
-    maxD = 10
+    E_den = -0.6:0.05:0.6
+    N = 80
+    len = size(E_den)[1]
+    # for i in 1:len
+    #     E0 = E_den[i]*N
+    #     @show "Running TMS with E0 = $E0"
+    #     run(E0, N, i)
+    # end
+    
+    Z2_overlap = zeros(len)
+    Elist = zeros(len)
+    for k in 1:len
+        f = h5open("data/N$(N)_$(k).h5", "r")
+        ψ = read(f, "ψ", MPS)
+        close(f)
+
+        sites = siteinds(ψ)
+        H = PXP_Hamiltonian(sites)
+        Z2 = Z2state(sites)
+
+        Elist[k] = inner(ψ', H, ψ)
+        Z2_overlap[k] = inner(ψ, Z2)
+    end
+
+    @printf("Now time for the plot\n")
+
+    Z2_overlap = abs.(Z2_overlap).^2
+    Elist = Elist ./ 20
+
+    fig = Figure(resolution=(800, 600))
+    ax = Axis(fig[1, 1],
+                xlabel = L"E/L",
+                ylabel = L"\log_{10} |\langle Z_2 | \Psi_n \rangle|^2",
+                title = "Overlap of TMS states with Z2 state")
+                scatter!(ax, E_den, log10.(Z2_overlap))
+                # save("data/TMS.pdf", fig)
+
+    return fig
+end
+
+function run(E0, N, iter)
+    maxD = 100
     minD = 20
-
-    @show "Generating random MPS with N = $N, maxD = $maxD, minD = $minD"
-
-    E0 = -1.1
     max_op = 100 # maximum number of iterations
 
     ξthre = 0.1 # threshold for updating ξ
@@ -28,7 +69,9 @@ function main()
     H = PXP_Hamiltonian(sites)
     @show H
     @show "Building Z2 state"
-    ψ0 = Z2state(sites)
+    # ψ0 = Z2state(sites)
+    states = [isodd(n) ? "Up" : "Dn" for n in 1:length(sites)]
+    ψ0 = randomMPS(sites, states, 2)
     @show ψ0
 
     ampo0 = OpSum()
@@ -97,10 +140,10 @@ function main()
             @printf("Updating ξ to %.12f\n", ξthre)
         end
 
-        mkpath("data")
-        f = h5open("data/N$(N)_$(k).h5", "w")
-        write(f, "ψ", ψ)
-        close(f)
+        # mkpath("data")
+        # f = h5open("data/N$(N)_$(k).h5", "w")
+        # write(f, "ψ", ψ)
+        # close(f)
 
         if varlist < ϵ
             @show "Converged with variance = $varlist < $ϵ"
@@ -116,6 +159,10 @@ function main()
     Elist = inner(ψ', H, ψ)
     varlist = inner(H, ψ, H, ψ) - Elist^2
     @printf("Final PXP E variance=%.12f,    E=%.12f\n",varlist ,Elist)
+    mkpath("data")
+    f = h5open("data/N$(N)_$(iter).h5", "w")
+    write(f, "ψ", ψ)
+    close(f)
 
     # return ψ, Elist, varlist
 
@@ -147,3 +194,40 @@ function Z2state(sites)
     return ψ
 end
 
+
+function checkData(N)
+    Z2_overlap = zeros(N+1)
+    varlist = zeros(N+1)
+    Elist = zeros(N+1)
+    S_ent = zeros(N+1)
+    for k in 1:5
+        f =h5open("data/N$(N)_$(k).h5", "r")
+        ψ = read(f, "ψ", MPS)
+        close(f)
+
+        sites = siteinds(ψ)
+        H = PXP_Hamiltonian(sites)
+        Z2 = Z2state(sites)
+
+        Elist[k] = inner(ψ', H, ψ)
+        varlist[k] = inner(H, ψ, H, ψ) - Elist[k]^2
+        Z2_overlap[k] = inner(ψ, Z2)
+
+        mid = Int(N/2)
+        orthogonalize!(ψ, mid)
+        ϕ = ψ[mid] * ψ[mid+1]
+        _, S = svd(ϕ, (linkind(ψ, mid-1), siteind(ψ, mid)))
+
+        SvN_initial = 0
+        for n in 1:dim(S, 1)
+            p = S[n, n]^2
+            if p>1e-10
+                SvN_initial -= p*log(p)
+            end
+        end
+        S_ent[k] = SvN_initial
+    end
+    for k in 1:5
+        @printf("k=%d, E=%.12f, var=%.12f, Z2_overlap=%.6f, S_ent=%.6f\n", k, Elist[k], varlist[k], Z2_overlap[k], S_ent[k])
+    end
+end
