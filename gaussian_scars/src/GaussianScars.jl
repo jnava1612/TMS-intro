31c84a393915f42e2828c27ccf1b6d64d4ace596
Jorge E Nava <navajorgeeduardo@gmail.com>
2026-04-17 13:36:34 -0300

    Updated all imports and exports

--- gaussian_scars/src/GaussianScars.jl
+++ gaussian_scars/src/GaussianScars.jl
@@ -12,26 +12,39 @@ using LaTeXStrings
 using CairoMakie
 using BenchmarkTools
 
+include("config.jl")
+include("gaussian_hamiltonian.jl")
 include("operator_actions.jl")
-# include("operators.jl")
-# include("gaussian_hamiltonian.jl")
-# include("pxp_model.jl")
-# include("states.jl")
-# include("scars.jl")
-# include("parameters.jl")
-# include("optimization.jl")
-# include("config.jl")
+include("optimization.jl")
+include("parameters.jl")
+include("plotting.jl")
+include("pxp_model.jl")
+include("scar_extraction.jl")
+include("states.jl")
 
-# export fermion_sign, annihilate, create, apply_cdag_cdag, apply_c_c, apply_cdag_c
-# export build_term_lists, bitwise_gaussian_action, gaussian_groundstate_bitwise
-# export verify_bitwise_action, full_c, gaussian_hamiltonian
-# export constrainedBasis, build_reversal_map, build_pxp_hamiltonian, build_projectors
-# export apply_spatial_inversion!, symmetrize_state, embed_to_full, project_to_fib
-# export entropy, scar_tower, view_scars
-# export pack_params, unpack_params, initial_params
-# export cost, optimize_gaussian
-# export OptimizationConfig, PlotConfig, BenchmarkConfig
-# export run_optimization, plot_initial_states, benchmark_gaussian_hamiltonian
+
+
+export OptimizationConfig, PlotConfig, BenchmarkConfig 
+export GaussianSolver, FullMatrixSolver, BitwiseSolver
+
+export build_term_lists, bitwise_gaussian_action, bitwise_gaussian_groundstate
+export verify_bitwise_action, full_c, gaussian_hamiltonian
+
+export fermion_sign, annihilate, create, apply_cdag_cdag, apply_c_c, apply_cdag_c
+
+export cost, optimize_gaussian, get_ground_state
+
+export pack_params, unpack_params, initial_params
+
+export mytheme
+
+export constrainedBasis, build_reversal_map, build_pxp_hamiltonian, build_projectors
+
+export entropy, scar_tower, view_scars
+
+export apply_spatial_inversion!, symmetrize_state, embed_to_full, project_to_fib
+
+export run_optimization, plot_initial_states, benchmark_gaussian_hamiltonian
 
 
 end
